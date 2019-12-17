58ebd153ad008a4d01a232fa8be6e2429ed86957
BillClinton <bclinton@appunto.net>
2019-12-17 01:21:24 -0500

    set up app function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require('firebase-functions');
+const express = require('express');
+var cors = require('cors');
+const apartmentRouter = require('./routers/apartment');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(apartmentRouter);
+
+app.get('/api/timestamp', (request, response) => {
+  response.send(`${Date.now()}`);
+});
+
+// respond with 404 if root is requested
+app.get('/api/', function(req, res) {
+  res.send(404);
+});
+
+exports.app = functions.https.onRequest(app);
