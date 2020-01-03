56d585de0be0ed9c663aa7f553fef24179d01adc
BillClinton <bclinton@appunto.net>
2020-01-02 20:01:42 -0500

    Remove log statements

--- functions/firestore/Hooks.js
+++ functions/firestore/Hooks.js
@@ -2,7 +2,6 @@ const pre = function(methodName, fn) {
   if (!this.hooks.pre) {
     this.hooks.pre = {};
   }
-  // console.log('vcalling static pre function');
   this.hooks['pre'][methodName] = fn;
 };
 
@@ -10,15 +9,10 @@ const runHooks = async function(hookType, methodName) {
   const hooks = this.constructor.hooks;
   if (hooks[hookType] && hooks[hookType][methodName]) {
     const fn = hooks[hookType][methodName];
-    // console.log(typeof fn);
-    // console.log(fn.constructor.name);
     if (typeof fn === 'function') {
-      // console.log('its a function');
       if (fn.constructor.name === 'AsyncFunction') {
-        // console.log('its async - waiting');
         await fn.call(this);
       } else {
-        // console.log('its normal');
         fn.call(this);
       }
     }
