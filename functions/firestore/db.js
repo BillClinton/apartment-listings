b12a4eb2933506f5f11ba4d91e9008d575192ea8
BillClinton <bclinton@appunto.net>
2020-01-06 02:28:21 -0500

    Add config vars, storage vars

--- functions/firestore/db.js
+++ functions/firestore/db.js
@@ -1,10 +1,22 @@
 const admin = require('firebase-admin');
 const DocumentStore = require('./DocumentStore');
 const serviceAccount = require('./serviceAccountKey.json');
-const databaseURL = 'https://apartment-listings-demo.firebaseio.com';
+const {
+  databaseURL,
+  storageBucketURL,
+  storageBucketName
+} = require('./dbConfig');
+
+// const databaseURL = 'https://apartment-listings-demo.firebaseio.com';
+// const storageBucket = 'https://apartment-listings-demo.firebaseio.com';
 
 const db = new DocumentStore(admin);
 
-db.connect(databaseURL, admin.credential.cert(serviceAccount));
+db.connect(
+  admin.credential.cert(serviceAccount),
+  databaseURL,
+  storageBucketURL,
+  storageBucketName
+);
 
 module.exports = db;
