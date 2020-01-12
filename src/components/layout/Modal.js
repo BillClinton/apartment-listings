63a04f624ededd0147079d017edb3da3ae5b288d
BillClinton <bclinton@appunto.net>
2020-01-12 17:11:04 -0500

    Add wrapper to modal

--- src/components/layout/Modal.js
+++ src/components/layout/Modal.js
@@ -4,14 +4,14 @@ import styles from './Modal.module.scss';
 import { ReactComponent as CloseIcon } from '../icons/times-circle-solid.svg';
 
 const Modal = props => {
-  console.log('modal');
-  console.log(props);
   return ReactDOM.createPortal(
-    <div className={styles.modal}>
-      <CloseIcon onClick={props.onDismiss} />
-      <div className={styles.header}>{props.title}</div>
-      <div className={styles.content}>{props.content}</div>
-      <div className={styles.actions}>{props.actions}</div>
+    <div className={styles.modalWrap}>
+      <div className={styles.modal}>
+        <CloseIcon onClick={props.onDismiss} />
+        <div className={styles.header}>{props.title}</div>
+        <div className={styles.content}>{props.content}</div>
+        <div className={styles.actions}>{props.actions}</div>
+      </div>
     </div>,
     document.querySelector('#modal')
   );
