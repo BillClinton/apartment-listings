1cf99926f4176173d72ef9f01fa80815f7eccb05
BillClinton <bclinton@appunto.net>
2020-01-12 17:13:08 -0500

    Close menu when clicked outside

--- src/components/layout/AdminHeader.js
+++ src/components/layout/AdminHeader.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './AdminHeader.module.scss';
 import { ReactComponent as BarsIcon } from '../icons/bars-solid.svg';
@@ -6,20 +6,30 @@ import { ReactComponent as CloseIcon } from '../icons/times-circle-solid.svg';
 
 const AdminHeader = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   const toggleClass = [styles.menuToggle];
+  const myRef = useRef();
+
+  const handleClickOutside = e => {
+    if (!myRef.current.contains(e.target)) {
+      setIsMenuOpen(false);
+    }
+  };
+
   if (isMenuOpen) {
     toggleClass.push(styles.menuToggleActive);
+    document.addEventListener('mousedown', handleClickOutside);
+  } else {
+    document.removeEventListener('mousedown', handleClickOutside);
   }
 
   return (
     <header className={styles.header}>
       <div className={styles.innerwrap}>
         <div className={styles.brand}>
-          <h1>Site Title</h1>
+          <h1>Site Title is</h1>
         </div>
 
-        <nav className={isMenuOpen ? styles.navActive : ''}>
+        <nav ref={myRef} className={isMenuOpen ? styles.navActive : ''}>
           <Link to="/">Home</Link>
           <Link to="/admin/apartments">Apartments</Link>
           <Link to="/admin/users">Users</Link>
